025cee42d0bb0e7633d90c82d6c0d12c472239c6
Azghour-Saad <saad.azghour@gmail.com>
2023-06-22 19:01:00 +0100

    Add DndProvider for react-dnd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <DndProvider backend={HTML5Backend}>
+        <Component {...pageProps} />
+      </DndProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
